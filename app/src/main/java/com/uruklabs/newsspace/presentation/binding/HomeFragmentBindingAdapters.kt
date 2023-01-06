f4e4aabfa92933db5de6e2ba57c7982dc5bad3c8
Leonardo Domingues <leodomiings@gmail.com>
2023-01-06 09:38:45 -0300

    style : aparencia do item da lista de noticias

--- app/src/main/java/com/uruklabs/newsspace/presentation/binding/HomeFragmentBindingAdapters.kt
+++ app/src/main/java/com/uruklabs/newsspace/presentation/binding/HomeFragmentBindingAdapters.kt
@@ -1,13 +1,13 @@
 package com.uruklabs.newsspace.presentation.binding
 
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
-import com.google.android.material.appbar.MaterialToolbar
 import com.uruklabs.newsspace.R
 import com.uruklabs.newsspace.data.SpaceFlightNewsCategory
 
-@BindingAdapter("toolbarTitle")
-fun MaterialToolbar.setToolbarTitleFromCategory(category: LiveData<SpaceFlightNewsCategory>?) {
+@BindingAdapter("textViewText")
+fun TextView.titleFromCategory(category: LiveData<SpaceFlightNewsCategory>?) {
 
     category?.let {
         val stringRes = when (it.value) {
@@ -17,7 +17,7 @@ fun MaterialToolbar.setToolbarTitleFromCategory(category: LiveData<SpaceFlightNe
                 R.string.latest_reports
             }
         }
-        this.title = context.getString(stringRes)
+        this.text = context.getString(stringRes)
     }
 
 }
