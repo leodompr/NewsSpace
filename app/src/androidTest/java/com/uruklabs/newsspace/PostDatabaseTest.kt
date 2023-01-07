5e86834004d57edf80675fb584a6beba8b4d5217
Leonardo Domingues <leodomiings@gmail.com>
2023-01-07 10:48:47 -0300

    feat: testes do Room em PostDatabaseTest.kt

--- app/src/androidTest/java/com/uruklabs/newsspace/PostDatabaseTest.kt
+++ app/src/androidTest/java/com/uruklabs/newsspace/PostDatabaseTest.kt
@@ -1,13 +1,21 @@
 package com.uruklabs.newsspace
 
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.uruklabs.newsspace.data.dao.PostDao
+import com.uruklabs.newsspace.data.dao.databse.PostDatabase
 import com.uruklabs.newsspace.data.entites.database.LaunchDB
 import com.uruklabs.newsspace.data.entites.database.PostDB
-import com.uruklabs.newsspace.data.entites.model.Post
 import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class PostDatabaseTest {
@@ -48,18 +56,45 @@ class PostDatabaseTest {
 
     }
 
+    private lateinit var dao: PostDao
+    private lateinit var postDatabase: PostDatabase
+
+
+    @Before
+    fun createDB() {
+        //instanciar o PostDataBase em memória
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        postDatabase = Room.inMemoryDatabaseBuilder(
+            context,
+            PostDatabase::class.java
+        ).build()
+
+        dao = postDatabase.dao
+    }
+
+
+    @After
+    @Throws(IOException::class)
+    fun closeDB() {
+        postDatabase.close()
+    }
+
     @Test
     fun deve_GravarPostsNoBancoDeDadosLocal_AoReceberListaDePosts() {
         //verificando se lista não esta vazia
         lateinit var result: List<PostDB>
-        result = dao.getListPosts().firts()
+        runBlocking {
+            result = dao.getListPosts().first()
+        }
         assertTrue(result.isEmpty())
 
         //salvando os dados no Database
-        dao.saveAll(listDbPosts)
+        runBlocking {
+            dao.saveAll(listDbPosts)
+            result = dao.getListPosts().first()
+        }
 
         //verifando se dao não esta vazio
-        result = dao.getListPosts().firts()
         assertTrue(result.isNotEmpty())
 
     }
@@ -67,8 +102,10 @@ class PostDatabaseTest {
     @Test
     fun deve_RetornarPostsCorretamente_AoLerBancoDeDadosLocal() {
         lateinit var result: PostDB
-        dao.saveAll(listDbPosts)
-        result = dao.getListPosts().first()[0]
+        runBlocking {
+            dao.saveAll(listDbPosts)
+            result = dao.getListPosts().first()[0]
+        }
         assertTrue(result.title == listDbPosts.first().title)
     }
 
@@ -76,9 +113,11 @@ class PostDatabaseTest {
     @Test
     fun deve_LimparBancoDeDadosLocal_AoChamarFuncaoCorrespondente() {
         lateinit var result: List<PostDB>
-        dao.saveAll(listDbPosts)
-        dao.clearDB()
-        result = dao.getListPosts().firts()
+        runBlocking {
+            dao.saveAll(listDbPosts)
+            dao.clearDB()
+            result = dao.getListPosts().first()
+        }
         assertTrue(result.isEmpty())
     }
 
--- app/src/main/java/com/uruklabs/newsspace/data/dao/databse/PostDatabase.kt
+++ app/src/main/java/com/uruklabs/newsspace/data/dao/databse/PostDatabase.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.uruklabs.newsspace.data.dao.PostDao
 import com.uruklabs.newsspace.data.entites.database.LaunchDB
 import com.uruklabs.newsspace.data.entites.database.PostDB
+import com.uruklabs.newsspace.data.entites.database.PostDBConverters
 
 @Database(entities = [PostDB::class, LaunchDB::class], version = 1, exportSchema = false)
+@TypeConverters(PostDBConverters::class)
 abstract class PostDatabase : RoomDatabase() {
     abstract val dao: PostDao
 
