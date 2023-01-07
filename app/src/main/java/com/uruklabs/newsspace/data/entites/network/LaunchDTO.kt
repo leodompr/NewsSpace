25d0e621a67250441b8206775d59f98b9eb56cda
Leonardo Domingues <leodomiings@gmail.com>
2023-01-07 10:57:43 -0300

    feat : converter do DTO para DB

--- app/src/main/java/com/uruklabs/newsspace/data/entites/network/LaunchDTO.kt
+++ app/src/main/java/com/uruklabs/newsspace/data/entites/network/LaunchDTO.kt
@@ -1,5 +1,6 @@
 package com.uruklabs.newsspace.data.entites.network
 
+import com.uruklabs.newsspace.data.entites.database.LaunchDB
 import com.uruklabs.newsspace.data.entites.model.Launch
 
 data class LaunchDTO(
@@ -11,8 +12,17 @@ data class LaunchDTO(
         provider = provider
     )
 
+    fun toDB(): LaunchDB = LaunchDB(
+        id = id,
+        provider = provider
+    )
+
 
 }
 fun Array<LaunchDTO>.toModel() : Array<Launch> = this.map {
     it.toModel()
+}.toTypedArray()
+
+fun Array<LaunchDTO>.toDB() : Array<LaunchDB> = this.map {
+    it.toDB()
 }.toTypedArray()
--- app/src/main/java/com/uruklabs/newsspace/data/entites/network/PostDTO.kt
+++ app/src/main/java/com/uruklabs/newsspace/data/entites/network/PostDTO.kt
@@ -1,5 +1,6 @@
 package com.uruklabs.newsspace.data.entites.network
 
+import com.uruklabs.newsspace.data.entites.database.PostDB
 import com.uruklabs.newsspace.data.entites.model.Launch
 import com.uruklabs.newsspace.data.entites.model.Post
 
@@ -23,8 +24,23 @@ data class PostDTO(
         updatedAt = updatedAt,
         launches = launches.toModel()
     )
+
+    fun toDB(): PostDB = PostDB(
+        id = id,
+        title = title,
+        url = url,
+        imageUrl = imageUrl,
+        summary = summary,
+        publishedAt = publishedAt,
+        updatedAt = updatedAt,
+        launches = launches.toDB()
+    )
 }
 
 fun List<PostDTO>.toModel(): List<Post> = this.map {
     it.toModel()
+}
+
+fun List<PostDTO>.toDB(): List<PostDB> = this.map {
+    it.toDB()
 }
