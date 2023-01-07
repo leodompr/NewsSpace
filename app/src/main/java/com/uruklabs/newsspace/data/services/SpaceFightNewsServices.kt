f2c5436efc0478d40bcfc78e10656f1449e7e475
Leonardo Domingues <leodomiings@gmail.com>
2023-01-07 08:04:45 -0300

    feat: criação dos metodos no SpaceFightNewsServices criados na classe de teste  (tdd)

--- app/src/main/java/com/uruklabs/newsspace/data/services/SpaceFightNewsServices.kt
+++ app/src/main/java/com/uruklabs/newsspace/data/services/SpaceFightNewsServices.kt
@@ -3,6 +3,7 @@ package com.uruklabs.newsspace.data.services
 import com.uruklabs.newsspace.data.model.Post
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SpaceFightNewsServices {
 
@@ -10,6 +11,12 @@ interface SpaceFightNewsServices {
 //    suspend fun listPosts() : List<Post>
 
     @GET("{type}")
-    suspend fun getListPosts(@Path("type") type : String) : List<Post>
+    suspend fun getListPosts(@Path("type") type: String): List<Post>
+
+    @GET("{type}")
+    suspend fun getListPostsByTitle(
+        @Path("type") type: String,
+        @Query("title_contains") query: String?
+    ): List<Post>
 
 }
